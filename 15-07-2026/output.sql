b3dc53331d2f4e78afb8791baef1596d3bde6154
lahari-sai972 <laharikora@gmail.com>
2026-07-15 21:48:28 +0530

    Create output.sql

---
+++ 15-07-2026/output.sql
@@ -0,0 +1,42 @@
+
+
+-- Query 1
+John      | HR
+Emma      | Finance
+David     | IT
+Sophia    | IT
+Michael   | Sales
+James     | Marketing
+Isabella  | Sales
+
+-- Query 2
+Payroll System     | HR
+Audit System       | Finance
+Website Redesign   | Marketing
+ERP Upgrade        | IT
+Sales Dashboard    | Sales
+Automation         | Operations
+
+-- Query 3
+John      | Payroll System
+Emma      | Audit System
+David     | ERP Upgrade
+David     | AI Chatbot
+Sophia    | ERP Upgrade
+Michael   | Sales Dashboard
+James     | Website Redesign
+William   | AI Chatbot
+Ava       | Audit System
+Isabella  | Sales Dashboard
+
+-- Query 4
+John      | 50000 | New York
+Emma      | 65000 | Chicago
+David     | 70000 | Dallas
+Sophia    | 72000 | Dallas
+Michael   | 55000 | Seattle
+James     | 60000 | Boston
+Isabella  | 68000 | Seattle
+
+-- Query 5
+Same as Query 3.
